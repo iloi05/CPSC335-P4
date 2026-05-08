2870a6f580b5570eab0feec8ae3cefff993750fb
root <root@DESKTOP-8969P1M.localdomain>
2026-05-07 19:03:26 -0700

    uhhhh code no good, need fix

---
+++ algo1exhaust.py
@@ -0,0 +1,22 @@
+def maxStocks(stocks, amount):
+    # output is supposed to be [10, [1, 3]]
+
+    bestCombo = []
+    # 1 to access the weight of the stock, n + 1 to access the next weight
+    for s in range(1, n + 1):
+        1 + (n + 1)
+        if 1 + (n + 1) != amount:
+            continue
+        if 1 + (n + 1) == amount:
+            bestCombo.append(1, n + 1)
+        
+    return amount, bestCombo
+
+def main():
+    stocks = [[1, 2], [3, 3], [5, 6], [6, 7]]
+    amount = 10
+
+if __name__ == "__main__":
+    main()
+
+
