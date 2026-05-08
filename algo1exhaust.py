632c628ce3d52de45d3b27e9eeb814504cf158e8
root <root@DESKTOP-8969P1M.localdomain>
2026-05-07 23:31:59 -0700

    algo1 exhaustive done, focused on weight only

--- algo1exhaust.py
+++ algo1exhaust.py
@@ -1,20 +1,24 @@
+from itertools import combinations
 def maxStocks(stocks, amount):
     # output is supposed to be [10, [1, 3]]
+    weight = [i[1] for i in stocks]
+    w = len(stocks)
 
-    bestCombo = []
-    # 1 to access the weight of the stock, n + 1 to access the next weight
-    for s in range(1, n + 1):
-        1 + (n + 1)
-        if 1 + (n + 1) != amount:
-            continue
-        if 1 + (n + 1) == amount:
-            bestCombo.append(1, n + 1)
-        
-    return amount, bestCombo
+    for stock in range(1, w + 1):
+        for combo in combinations(range(w), stock):
+            totW = sum(weight[i] for i in combo)
+
+            if totW == amount:
+                return [amount, list(combo)]
+
+    return [0, []]    
+    
+
 
 def main():
     stocks = [[1, 2], [3, 3], [5, 6], [6, 7]]
     amount = 10
+    print(maxStocks(stocks, amount))
 
 if __name__ == "__main__":
     main()
